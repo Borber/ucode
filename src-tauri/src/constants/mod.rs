e69577be1678417d5a06575d422f45d79f3348c0
Borber <borber.drum@gmail.com>
2022-09-07 03:17:54 +0800

    [mod] 添加部分注释和修改代码块传值结构

--- src-tauri/src/constants/mod.rs
+++ src-tauri/src/constants/mod.rs
@@ -3,8 +3,9 @@ use std::string::ToString;
 
 use once_cell::sync::Lazy;
 
+// TODO 后期提供统计数量进行排序/固定顺序/或其他顺序
 pub static LAN: Lazy<Vec<String>> = Lazy::new(||
-    vec!["c", "java", "rust"]
+    vec!["c", "cpp", "java", "rust"]
         .iter()
         .map(|s| s.to_string())
         .collect()
--- src-tauri/src/model/code.rs
+++ src-tauri/src/model/code.rs
@@ -1,9 +1,14 @@
 use serde::Deserialize;
 
+/// 代码片段主体
 #[derive(Clone, Debug, Deserialize)]
 pub struct Code {
-    pub id: Option<i64>,
+    // 用途描述
     pub desc: Option<String>,
-    pub lan: Option<i64>,
+    // 语言类型
+    pub lan: Option<String>,
+    // 标签
     pub tags: Option<Vec<i64>>,
+    // 主体
+    pub body: Option<String>,
 }
